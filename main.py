c65ea5b9900a14742fa85f2062e67b4f3c1a4092
Зубайраева Ольга <vadelevich2013@yandex.ru>
2022-12-23 19:36:06 +0300

    add main

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+from connector import Connector
+from  engine_classes import HH, SuperJob
+from jobs_classes import HHVacancy,SJVacancy
+from  jobs_classes import get_top,sorting
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Введите число сайта с которым хотите работать")
+    vvod = int(input(' 1 : https://hh.ru/, 2 : https://api.superjob.ru/'))
+    vvod_word = input("Введите ключевое слово для поиска :")
+    vvod_vacancies_count = int(input('Сколько нужно вывести вакакнсий ?'))
+    while True:
+        search_word = vvod_word
+        count = vvod_vacancies_count
+        vacancies_count = 100
+        if vvod == 1:
+            hh_engin = HH()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            resultHH = hh_engin.get_request(search_word, vacancies_count)
+            a = HH.get_connector('resultHH.json')
+            a.insert(resultHH)
+            HHVacancy.instantiate_from_json('resultHH.json')
+            get_top(HHVacancy.vacancies, count)
+            print(HHVacancy.get_count_of_vacancy)
+
+        if vvod == 2:
+            sj_engin = SuperJob()
+            resultSJ = sj_engin.get_request(search_word, vacancies_count)
+            b = SuperJob.get_connector('resultSJ.json')
+            b.insert(resultSJ)
+            SJVacancy.instantiate_from_json('resultSJ.json')
+            sorting(SJVacancy.vacancies)
+            get_top(SJVacancy.vacancies,count)
+
+        break
+
